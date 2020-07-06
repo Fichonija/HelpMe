1df0c8352525ceb48e5fb006b840476dd354b963
Fichonija <bfilip9205@gmail.com>
2020-07-06 18:49:27 +0200

    Add routing.

--- backend/app.js
+++ backend/app.js
@@ -1,52 +1,65 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const Post = require('./models/post');
+const Post = require("./models/post");
 
 const app = express();
 
-
-mongoose.connect('mongodb+srv://fbolcic:C8aW72Pn8eyjFRjf@cluster0-eijik.mongodb.net/HelpMe?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => { console.log('Connected to MongoDB.') })
-  .catch((err) => { console.log('Connection to MongoDB failed. Error: ' + err) });
+mongoose
+  .connect(
+    "mongodb+srv://fbolcic:C8aW72Pn8eyjFRjf@cluster0-eijik.mongodb.net/HelpMe?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB.");
+  })
+  .catch((err) => {
+    console.log("Connection to MongoDB failed. Error: " + err);
+  });
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
   const post = new Post({
     title: req.body.title,
-    summary: req.body.summary
+    summary: req.body.summary,
   });
   post.save();
 
   console.log(post);
   res.status(201).json({
-    message: 'Post added successfuly.'
-  });
-})
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find().then(documents => {
-      res.status(200).json({
-        message: 'Posts fetched.',
-        data: documents
-      });
+    message: "Post added successfuly.",
   });
 });
 
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Post ' + req.params.id + ' deleted.' });
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched.",
+      data: documents,
+    });
   });
-})
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({ message: "Post " + req.params.id + " deleted." });
+  });
+});
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { MatCardModule } from "@angular/material/card";
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { AppComponent } from './app.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
-import { HeaderComponent } from './header/header.component';
+import { AppComponent } from "./app.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostListComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, PostListComponent, HeaderComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    AppRoutingModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
