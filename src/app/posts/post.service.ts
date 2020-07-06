e6ab33c80f36b11efe0240ab4ddf2a15d83c2d9b
Fichonija <bfilip9205@gmail.com>
2020-07-06 20:05:56 +0200

    Add getPost in PostService.

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,41 +1,68 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { Subject, Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
-import { Post } from './post.model';
-import { HttpClient } from '@angular/common/http';
+import { Post } from "./post.model";
+import { HttpClient } from "@angular/common/http";
 
-@Injectable({providedIn: 'root'})
-export class PostService{
+@Injectable({ providedIn: "root" })
+export class PostService {
+  private postEndpoint: string = "http://localhost:3000/api/posts";
 
   private posts: Post[] = [];
   private postsUpdated: Subject<Post[]> = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {};
+  constructor(private http: HttpClient) {}
 
-  getPostsUpdatedListener(): Observable<Post[]>{
-
+  getPostsUpdatedListener(): Observable<Post[]> {
     return this.postsUpdated.asObservable();
   }
 
-  getPosts(): void{
-    this.http.get<{message: string, data: any}>('http://localhost:3000/api/posts')
-    .pipe(map(res => {
-      return res.data.map(post => { return { id: post._id, title: post.title, summary: post.summary } })
-    }))
-    .subscribe((posts) => {
+  getPosts(): void {
+    this.http
+      .get<{ message: string; data: any }>(this.postEndpoint)
+      .pipe(
+        map((res) => {
+          return res.data.map((post) => {
+            return this.normalizePost(post);
+          });
+        })
+      )
+      .subscribe((posts) => {
         this.posts = posts;
         this.postsUpdated.next([...this.posts]);
-    });
+      });
   }
 
-  addPost(post: Post): void{
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post).subscribe(
-      (res) => {
+  getPost(id: string): void {
+    this.http
+      .get<{ message: string; data: any }>(this.postEndpoint + "/" + id)
+      .pipe(
+        map((res) => {
+          return res.data.map((post) => {
+            return this.normalizePost(post);
+          });
+        })
+      );
+  }
+
+  addPost(post: Post): void {
+    this.http
+      .post<{ message: string }>(this.postEndpoint, post)
+      .subscribe((res) => {
         console.log(res.message);
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
-      }
-    );
+      });
+  }
+
+  normalizePost(apiPost: any): Post {
+    return {
+      id: apiPost._id,
+      title: apiPost.title,
+      summary: apiPost.summary,
+      slug: apiPost.slug,
+      content: apiPost.content,
+    };
   }
 }
