5b0cf0032e9b8f6c4e1671bc56361beac11ad8c7
Fichonija <bfilip9205@gmail.com>
2020-07-06 20:06:14 +0200

    Update PostDetailComponent navigation.

--- src/app/posts/post-detail/post-detail.component.ts
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -1,7 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { PostService } from "../post.service";
+import { Post } from "../post.model";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-post-detail",
   templateUrl: "./post-detail.component.html",
 })
-export class PostDetailComponent {}
+export class PostDetailComponent implements OnInit {
+  post: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
+
+  ngOnInit() {
+    // this.postService.getPosts();
+  }
+}
