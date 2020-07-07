5c6fcd1367f1d61fd365c72d494eed96e2968c3c
Fichonija <bfilip9205@gmail.com>
2020-07-07 18:20:40 +0200

    Added Post routes.

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const Post = require("./models/post");
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -33,50 +33,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    summary: req.body.summary,
-    slug: req.body.slug,
-    content: req.body.content,
-  });
-  post.save();
-
-  console.log(post);
-  res.status(201).json({
-    message: "Post added successfuly.",
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find(req.query).then((documents) => {
-    res.status(200).json({
-      message: "Posts fetched.",
-      data: documents,
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.find({ _id: req.params.id }).then((document) => {
-    if (document) {
-      res.status(200).json({
-        message: "Post fetched.",
-        data: document,
-      });
-    } else {
-      res.status(404).json({
-        message: "Post not found.",
-      });
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post " + req.params.id + " deleted." });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
---
+++ backend/routes/posts.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+const Post = require("../models/post");
+
+const router = express.Router();
+
+router.post("", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    summary: req.body.summary,
+    slug: req.body.slug,
+    content: req.body.content,
+  });
+  post.save();
+
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfuly.",
+  });
+});
+
+router.get("", (req, res, next) => {
+  Post.find(req.query).then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched.",
+      data: documents,
+    });
+  });
+});
+
+router.get("/:id", (req, res, next) => {
+  Post.find({ _id: req.params.id }).then((document) => {
+    if (document) {
+      res.status(200).json({
+        message: "Post fetched.",
+        data: document,
+      });
+    } else {
+      res.status(404).json({
+        message: "Post not found.",
+      });
+    }
+  });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({ message: "Post " + req.params.id + " deleted." });
+  });
+});
+
+module.exports = router;
