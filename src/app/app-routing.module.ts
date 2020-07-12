9b82604764b85c90bcbec92fde7aa74cb7ba578e
Fichonija <bfilip9205@gmail.com>
2020-07-12 11:20:46 +0200

    Add Login Component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { PostDetailComponent } from "./posts/post-detail/post-detail.component";
+import { LoginComponent } from "./auth/login/login.component";
 
 const routes: Routes = [
+  { path: "login", component: LoginComponent },
   { path: "posts", component: PostListComponent },
   { path: "posts/:slug", component: PostDetailComponent },
   { path: "", component: PostListComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from "@angular/material/card";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
@@ -14,20 +16,24 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
 import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { PostDetailComponent } from "./posts/post-detail/post-detail.component";
+import { LoginComponent } from "./auth/login/login.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    LoginComponent,
     PostListComponent,
     PostDetailComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatCardModule,
     MatToolbarModule,
     MatButtonModule,
