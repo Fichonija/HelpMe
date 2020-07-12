d89725bddd7b7cbef191e667e63fe235de12b251
Fichonija <bfilip9205@gmail.com>
2020-07-12 18:48:06 +0200

    Add Login method on AuthService.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { AuthData } from "./auth.model";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+  private authEndpoint: string = "http://localhost:3000/api/auth";
+
   constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    const authData: AuthData = { email: email, password: password };
+    console.log(authData);
+    this.http.post(this.authEndpoint + "/login", authData).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
