7c01f33732d64a1fda37b603ef9014e516b1ab72
Fichonija <bfilip9205@gmail.com>
2020-07-22 20:22:50 +0200

    Add content for GoalsComponent.

--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -1,10 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import {
+  MatVerticalStepper,
+  MatHorizontalStepper,
+} from "@angular/material/stepper";
 
 @Component({
   selector: "app-goals",
   templateUrl: "./goals.component.html",
   styleUrls: ["./goals.component.css"],
 })
-export class GoalsComponent {
+export class GoalsComponent implements OnInit {
   public isLinear: boolean = false;
+  public page: string = "";
+
+  public goal: string = "";
+  public specific: string = "";
+  public measurable: string = "";
+  public achievable: string = "";
+  public relevant: string = "";
+  public timebound: string = "";
+
+  ngOnInit() {
+    this.reset();
+  }
+
+  showPage(page: string) {
+    this.page = page;
+  }
+
+  reset(startOnPage = "introduction") {
+    this.showPage(startOnPage);
+
+    this.goal = "";
+    this.specific = "";
+    this.measurable = "";
+    this.achievable = "";
+    this.relevant = "";
+    this.timebound = "";
+  }
 }
